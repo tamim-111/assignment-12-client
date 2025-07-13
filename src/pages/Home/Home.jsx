dba389678a8322515873519d1da2a0892e9fd90e
tamim-111 <tamim144362@gmail.com>
2025-07-13 06:29:59 +0600

    feat(home page): add FAQ section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import FAQ from './FAQ'
 import LatestNews from './LatestNews'
 
 const Home = () => {
   return (
     <div>
       <LatestNews></LatestNews>
+      <FAQ></FAQ>
     </div>
   )
 }
